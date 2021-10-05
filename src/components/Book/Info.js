3984bb5ab90dcdb101fcf5d457639bd43c3def3b
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-10-05 14:03:34 +0100

    Add data to Info component through props.

--- src/components/Book/Info.js
+++ src/components/Book/Info.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Info = () => (
-  <div className="info">
-    <p className="categorie">action</p>
-    <p className="title" />
-    <p className="author">Author</p>
-    <div className="creButtons">
-      <p className="comments">Comments</p>
-      <p className="remove">Remove</p>
-      <p className="edit">Edit</p>
+const Info = (props) => {
+  const { bookInfo } = props;
+  return (
+    <div className="info">
+      <p className="category">{bookInfo.category}</p>
+      <p className="title">{bookInfo.title}</p>
+      <p className="author">{bookInfo.author}</p>
+      <div className="creButtons">
+        <button type="button" className="comments">Comments</button>
+        <button type="button" className="remove">Remove</button>
+        <button type="button" className="edit">Edit</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
+Info.propTypes = {
+  bookInfo: PropTypes.shape.isRequired,
+};
 export default Info;
