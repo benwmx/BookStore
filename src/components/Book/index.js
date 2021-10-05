9bae73c62be186907dcb3d556c47f6907c1448a4
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-10-05 14:03:34 +0100

    Add data to Info components through props.

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Info from './Info';
 import Progress from './Progress';
 import Chapter from './Chapter';
 
-const Book = () => (
-  <div className="bookInfoContanier">
-    <Info />
-    <Progress />
-    <span className="verticalSeparator"> </span>
-    <Chapter />
-  </div>
-);
+const Book = (props) => {
+  const { bookInfo } = props;
+  return (
+    <div className="bookInfoContanier">
+      <Info bookInfo={bookInfo} />
+      <Progress />
+      <div className="verticalSeparator"> </div>
+      <Chapter />
+    </div>
+  );
+};
+
+Book.propTypes = {
+  bookInfo: PropTypes.shape.isRequired,
+};
 
 export default Book;
--- src/components/Books.js
+++ src/components/Books.js
@@ -3,12 +3,31 @@ import Book from './Book';
 import AddBook from './AddBook';
 
 const Books = () => {
-  const books = [];
+  const books = [
+    {
+      title: 'Books',
+      author: 'Rachid',
+      category: 'Action',
+    },
+    {
+      title: 'Books',
+      author: 'Rachid',
+      category: 'Action',
+    },
+    {
+      title: 'Books',
+      author: 'Rachid',
+      category: 'Action',
+    },
+  ];
   return (
     <>
       <div className="booksContainer">
         {books.map((book) => (
-          <Book key={book.id} />
+          <Book
+            key={book.id}
+            bookInfo={book}
+          />
         ))}
       </div>
       <div className="horizontalSeparator"> </div>
