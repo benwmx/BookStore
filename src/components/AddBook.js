a2774e9f457fcd7c3ba795d4c99b135fd011feb0
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-10-06 07:47:53 +0100

    Add onChange event to book form inputs.

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddBook = () => {
   const categories = [];
+  const [book, setBook] = useState({ id: '', title: '', author: '' });
+
+  const onChange = (event) => {
+    setBook({
+      ...book,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const submitBookToStore = (event) => {
+    event.preventDefault();
+    console.log(book);
+  };
   return (
     <>
       <h1 className="header">Add new book </h1>
-      <form>
-        <input type="text" className="bookTitle" placeholder="Book Title" />
-        <input type="text" className="bookAuthor" placeholder="Book Author" />
+      <form onSubmit={submitBookToStore}>
+        <input type="text" className="bookTitle" placeholder="Book Title" name="title" value={book.title} onChange={onChange} />
+        <input type="text" className="bookAuthor" placeholder="Book Author" name="author" value={book.author} onChange={onChange} />
         <select name="categorie" id="categorie">
           {categories.map((category) => (
             <option key={category.id} value={category.name}>{category.name}</option>
