fab7a81e32cc1ceb8ebebac5c6e2ecd03641d366
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-10-07 13:00:39 +0100

    Add thunk middleware for getting books from the Api.

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-restricted-syntax */
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const ADD_LIST_OF_BOOKS_FROM_API = 'bookstore/books/ADD_LIST_OF_BOOKS_FROM_API';
 
 const initialState = [];
 const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/i0GsPAoEktSI0pl0yKFK/books/';
@@ -9,8 +11,14 @@ const addBook = (payload) => ({
   payload,
 });
 
+const addListOfBooksFromApi = (payload) => ({
+  type: ADD_LIST_OF_BOOKS_FROM_API,
+  payload,
+});
+
+// redux thunks middlewares for adding/getting/removing books from the API.
 const addBookToApi = (payload) => (dispatch) => {
-  const book = { item_id: payload.id, title: payload.title, category: payload.category };
+  const book = { item_id: payload.id, title: `${payload.title} #author# ${payload.author}`, category: payload.category };
   fetch(url, {
     method: 'POST',
     headers: {
@@ -22,15 +30,43 @@ const addBookToApi = (payload) => (dispatch) => {
   });
 };
 
+const getListOfBooks = () => (dispatch) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => dispatch(addListOfBooksFromApi(data)));
+};
+//
 const removeBook = (payload) => ({
   type: REMOVE_BOOK,
   payload,
+});
+
+const separateTitleFromAuthor = (titleAuthor) => ({
+  title: titleAuthor.split('#author#')[0],
+  author: titleAuthor.split('#author#')[1],
 });
 
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK: return [...state, action.payload];
     case REMOVE_BOOK: return state.filter((book) => book.id !== action.payload);
+    case ADD_LIST_OF_BOOKS_FROM_API: {
+      const bookList = [];
+      const rawData = action.payload;
+      for (const id in rawData) {
+        if (Object.prototype.hasOwnProperty.call(rawData, id)) {
+          const { category, title } = rawData[id][0];
+          const titleAuthor = separateTitleFromAuthor(title);
+          const book = {
+            id,
+            category,
+            ...titleAuthor,
+          };
+          bookList.push(book);
+        }
+      }
+      return bookList;
+    }
     default: return state;
   }
 };
@@ -40,4 +76,5 @@ export {
   addBook,
   removeBook,
   addBookToApi,
+  getListOfBooks,
 };
