3a21327466f6cc5b1827e27fa3f4006196f36dcd
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2021-10-07 11:28:54 +0100

    Add controlled select for categories.

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,26 +1,33 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook } from '../redux/books/books';
+import { addBookToApi } from '../redux/books/books';
 
 const AddBook = () => {
-  const categories = [];
-  const [book, setBook] = useState({ id: '', title: '', author: '' });
+  const categories = [{ id: '1', name: 'Fantasy' }, { id: '2', name: 'Science Fiction' }];
+  const [book, setBook] = useState({
+    id: '', title: '', author: '', category: 'Fantasy',
+  });
+
+  const [selectedCategory, setCategory] = useState('Fantasy');
+
   const dispatch = useDispatch();
 
   const onChange = (event) => {
-    setBook({
-      ...book,
+    if (event.target.name === 'category') setCategory(event.target.value);
+    setBook((oldBook) => ({
+      ...oldBook,
       [event.target.name]: event.target.value,
-    });
+    }));
   };
 
   const submitBookToStore = (event) => {
     event.preventDefault();
     if (book.title.trim() !== '' && book.author.trim() !== '') {
       const newBook = { ...book, id: uuidv4() };
-      dispatch(addBook(newBook));
-      setBook({ title: '', author: '' });
+      dispatch(addBookToApi(newBook));
+      setBook({ title: '', author: '', category: 'Fantasy' });
     }
   };
   return (
@@ -29,7 +36,7 @@ const AddBook = () => {
       <form onSubmit={submitBookToStore}>
         <input type="text" className="bookTitle" placeholder="Book Title" name="title" value={book.title} onChange={onChange} required />
         <input type="text" className="bookAuthor" placeholder="Book Author" name="author" value={book.author} onChange={onChange} required />
-        <select name="categorie" id="categorie">
+        <select name="category" id="category" value={selectedCategory} onChange={onChange} required>
           {categories.map((category) => (
             <option key={category.id} value={category.name}>{category.name}</option>
           ))}
